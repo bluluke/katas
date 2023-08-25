d614c0153d9ab26a0772edaacc0a2335e24cabaa
bluluke <bluluke@hotmail.com>
2023-08-25 08:57:48 +0100

    Update: findOutlier function return 13 from 13 from

--- javascript/__tests__/findOutlier.test.js
+++ javascript/__tests__/findOutlier.test.js
@@ -1,1 +1,17 @@
 const { findOutlier } = require('../findOutlier'); 
+
+describe('findOutlier', () => { 
+  test('Should return the single odd number from array of 3 numbers', () => { 
+    //arrange 
+    const expected1 = 13; 
+    const expected2 = 101; 
+    const expected3 = 1; 
+    const input1 = [10, 13, 20]; 
+    const input2 = [18, 14, 25, 30, 40]; 
+    const input3 = [99, 10, 20, 100, 10000]; 
+    //act 
+    const actual1 = findOutlier(input1); 
+    //assert 
+    expect(actual1).toEqual(expected1); 
+  }); 
+});
--- javascript/findOutlier.js
+++ javascript/findOutlier.js
@@ -1,5 +1,18 @@
-const findOutlier = () => {
+const findOutlier = (numsArray) => {
 
+const oddArray = []
+const evenArray = [];
+
+numsArray.forEach((num) => {
+    if(num % 2 === 0) {
+        evenArray.push(num)
+    } else {
+        oddArray.push(num)
+    }
+})
+if(oddArray.length === 1) {
+    return oddArray[0];
+}
 }
 
 
