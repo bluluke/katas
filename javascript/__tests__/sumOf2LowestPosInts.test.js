f6342009ead6c4ba741a23d6a734d3460b1be863
bluluke <bluluke@hotmail.com>
2023-08-26 09:32:29 +0100

    Update: sumOf2LowestPoInts return sum of 2 numbers  from array of 2 numbers

--- javascript/__tests__/sumOf2LowestPosInts.test.js
+++ javascript/__tests__/sumOf2LowestPosInts.test.js
@@ -1,1 +1,13 @@
 const { sumOf2LowestPosInts} = require('../sumOf2LowestPosInts');
+
+describe('Should return ', () => { 
+  test('Should return the sum of 2 numbers from an array of 2 positive integers', () => { 
+    //arrange 
+    const expected = 8; 
+    const input = [5, 3]; 
+    //act 
+    const actual = sumOf2LowestPosInts(input); 
+    //assert 
+    expect(actual).toBe(expected); 
+  }); 
+});
--- javascript/sumOf2LowestPosInts.js
+++ javascript/sumOf2LowestPosInts.js
@@ -1,5 +1,5 @@
-const sumOf2LowestPosInts = () => {
-
+const sumOf2LowestPosInts = (numArray) => {
+    return numArray[0] + numArray[1];
 }
 
 
