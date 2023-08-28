e0f5c73c34335298e4eece49f8047a88513e3ee0
bluluke <bluluke@hotmail.com>
2023-08-28 09:47:04 +0100

    Update: isArmstrongNum return false from input of 33, 208, 4003, 6894, 9043

--- javascript/__tests__/isArmstrongNum.test.js
+++ javascript/__tests__/isArmstrongNum.test.js
@@ -49,4 +49,25 @@ describe.only('isArmstrongNum', () => {
     expect(actual4).toBe(expected); 
     expect(actual5).toBe(expected); 
 });
+test('Should return false from input of 33, 208, 4003, 6894, 9043 ', () => { 
+  //arrange 
+  const expected = false; 
+  const input1 = 33;
+    const input2 = 208;
+    const input3 = 4003;
+    const input4 = 6894;
+    const input5 = 9043; 
+    //act 
+    const actual1 = isArmstrongNum(input1); 
+    const actual2 = isArmstrongNum(input2); 
+    const actual3 = isArmstrongNum(input3); 
+    const actual4 = isArmstrongNum(input4); 
+    const actual5 = isArmstrongNum(input5); 
+    //assert 
+    expect(actual1).toBe(expected); 
+    expect(actual2).toBe(expected); 
+    expect(actual3).toBe(expected); 
+    expect(actual4).toBe(expected); 
+    expect(actual5).toBe(expected);  
+});
 });
