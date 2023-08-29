412a40c9489f552119ff78e69c6c62d6a81db463
bluluke <bluluke@hotmail.com>
2023-08-29 08:53:58 +0100

    Update: Maskify return string input unchanged if input length is less than 5.

--- javascript/__tests__/maskify.test.js
+++ javascript/__tests__/maskify.test.js
@@ -10,6 +10,42 @@ describe('Makify', () => {
     //assert 
     expect(actual).toBe(expected); 
   }); 
+  test('Should return string input unchanged if input length is less than 5.', () => { 
+    //arrange 
+    const expected1 = 'jf6'; 
+    const expected2 = '4'; 
+    const expected3 = 'jkvh'; 
+
+    const input1 = 'jf6'; 
+    const input2 = '4'; 
+    const input3 = 'jkvh'; 
+    //act 
+    const actual1 = maskify(input1); 
+    const actual2 = maskify(input2); 
+    const actual3 = maskify(input3); 
+    //assert 
+    expect(actual1).toBe(expected1); 
+    expect(actual2).toBe(expected2); 
+    expect(actual3).toBe(expected3); 
+  });
 });
 
 
+
+
+
+    // const expected = ; 
+    // const expected = ; 
+    // const expected = ; 
+
+    // const input = ; 
+    // const input = ; 
+    // const input = ; 
+    // //act 
+    // const actual = maskify(input); 
+    // const actual = maskify(input); 
+    // const actual = maskify(input); 
+    // //assert 
+    // expect(actual).toBe(expected); 
+    // expect(actual).toBe(expected); 
+    // expect(actual).toBe(expected); 
--- javascript/maskify.js
+++ javascript/maskify.js
@@ -1,6 +1,8 @@
 const maskify = (password) => {
     if( password === '') {
         return '';
+    } else if (password.length < 5) {
+        return password;
     }
 }
 
