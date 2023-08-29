9d3de18773e6a3f1fe48f3e75a69b4d6ae08d81a
bluluke <bluluke@hotmail.com>
2023-08-29 09:12:43 +0100

    Update: maskify return string input with all characters replaced with # except for last 4.

--- javascript/__tests__/maskify.test.js
+++ javascript/__tests__/maskify.test.js
@@ -28,6 +28,23 @@ describe('Makify', () => {
     expect(actual2).toBe(expected2); 
     expect(actual3).toBe(expected3); 
   });
+  test('Should return string input with all characters replaced with # except for last 4', () => { 
+    const expected1 = "####fghd"; 
+    const expected2 ="#0976"; 
+    const expected3 = "#####jdhf"; 
+
+    const input1 = "hduefghd"; 
+    const input2 = "30976"; 
+    const input3 = "wirozjdhf"; 
+    //act 
+    const actual1 = maskify(input1); 
+    const actual2 = maskify(input2); 
+    const actual3 = maskify(input3); 
+    //assert 
+    expect(actual1).toBe(expected1); 
+    expect(actual2).toBe(expected2); 
+    expect(actual3).toBe(expected3); 
+  });
 });
 
 
--- javascript/maskify.js
+++ javascript/maskify.js
@@ -4,6 +4,9 @@ const maskify = (password) => {
     } else if (password.length < 5) {
         return password;
     }
+    const last4Charts = password.slice(-4);
+    const firstPart = password.slice(0, -4).replace(/./g, '#');
+    return `${firstPart}${last4Charts}`
 }
 
 
