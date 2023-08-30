60015b2a110ef530afc45d4720aadc0a0460cc4c
bluluke <bluluke@hotmail.com>
2023-08-30 10:35:36 +0100

    Update: categoriseMembers return ['open'] from input of [[18, 3]].

--- javascript/__tests__/categoriseMembers.test.js
+++ javascript/__tests__/categoriseMembers.test.js
@@ -1,1 +1,13 @@
 const { categoriseMembers } = require('../categoriseMembers');
+
+describe('categoriseMembers', () => { 
+  test('should return array of single item "open" from input of array containing nested array: [18, 3] ', () => { 
+    //arrange 
+    const expected = ['open']; 
+    const input = [[18, 3]]; 
+    //act 
+    const actual = categoriseMembers(input); 
+    //assert 
+    expect(actual).toEqual(expected); 
+  }); 
+});
--- javascript/categoriseMembers.js
+++ javascript/categoriseMembers.js
@@ -1,5 +1,9 @@
-const categoriseMembers = () => {
-
+const categoriseMembers = (membersArray) => {
+    let memberCategorisations = [];
+    membersArray.forEach((memeber) => {
+        memberCategorisations.push('open')
+    })
+    return memberCategorisations
 }
 
 
