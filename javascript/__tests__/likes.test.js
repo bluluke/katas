dab89cf2f7d6f30c2efc8251ed20c0540f83c8f9
bluluke <bluluke@hotmail.com>
2023-08-31 10:15:15 +0100

    Update: likes return 'no one likes this' from input of empty array.

--- javascript/__tests__/likes.test.js
+++ javascript/__tests__/likes.test.js
@@ -1,5 +1,13 @@
 const { likes } = require('../likes')
 
 describe('Likes', () => { 
-  
+    test('Should return message no one likes this from input of empty array', () => { 
+    //arrange 
+    const expected = 'no one likes this'; 
+    const input = []; 
+    //act 
+    const actual = likes(input); 
+    //assert 
+    expect(actual).toBe(expected); 
+    });
 });
--- javascript/likes.js
+++ javascript/likes.js
@@ -1,5 +1,7 @@
-const likes = () => {
-
+const likes = (peopleWhoLikeThis) => {
+    if(peopleWhoLikeThis.length === 0) {
+        return 'no one likes this'
+    }
 }
 
 
