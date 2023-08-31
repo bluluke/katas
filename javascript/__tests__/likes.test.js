bcfeaaf5b65a52df9fe6eaf44b5d2b432195dec5
bluluke <bluluke@hotmail.com>
2023-08-31 10:28:53 +0100

    Update: likes return array of input array item 1 and input array item 2 from input array of 2 strings .

--- javascript/__tests__/likes.test.js
+++ javascript/__tests__/likes.test.js
@@ -19,4 +19,21 @@ describe('Likes', () => {
       //assert 
       expect(actual).toBe(expected); 
     });
+    test('Should return (input array item 1) and (input array item 2) like this from input of array containing 2 strings', () => { 
+      //arrange 
+      const expected1 = "Frank and Barbera like this"; 
+      const expected2 = "Sarah and Jessica like this"; 
+      const expected3 = "Paul and Deborah like this"; 
+      const input1 = ["Frank", "Barbera"]; 
+      const input2 = ['Sarah', 'Jessica']; 
+      const input3 = ['Paul', 'Deborah']; 
+      //act 
+      const actual1 = likes(input1); 
+      const actual2 = likes(input2); 
+      const actual3 = likes(input3); 
+      //assert 
+      expect(actual1).toBe(expected1); 
+      expect(actual2).toBe(expected2);
+      expect(actual3).toBe(expected3);
+    });
 });
--- javascript/likes.js
+++ javascript/likes.js
@@ -3,6 +3,8 @@ const likes = (peopleWhoLikeThis) => {
         return 'no one likes this'
     } else if(peopleWhoLikeThis.length === 1) {
         return `${peopleWhoLikeThis[0]} likes this`
+    } else if (peopleWhoLikeThis.length === 2) {
+        return `${peopleWhoLikeThis[0]} and ${peopleWhoLikeThis[1]} like this`
     }
 }
 
