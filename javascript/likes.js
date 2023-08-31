c858f1579a88417be68f649ca59483508e7088cb
bluluke <bluluke@hotmail.com>
2023-08-31 11:03:25 +0100

    Refactor: likes with switch statement.

--- javascript/likes.js
+++ javascript/likes.js
@@ -1,14 +1,20 @@
 const likes = (peopleWhoLikeThis) => {
-    if(peopleWhoLikeThis.length === 0) {
-        return 'no one likes this'
-    } else if(peopleWhoLikeThis.length === 1) {
-        return `${peopleWhoLikeThis[0]} likes this`
-    } else if (peopleWhoLikeThis.length === 2) {
-        return `${peopleWhoLikeThis[0]} and ${peopleWhoLikeThis[1]} like this`
-    } else if(peopleWhoLikeThis.length === 3) {
-        return `${peopleWhoLikeThis[0]}, ${peopleWhoLikeThis[1]} and ${peopleWhoLikeThis[2]} like this`
-    } else {
-        return `${peopleWhoLikeThis[0]}, ${peopleWhoLikeThis[1]} and ${peopleWhoLikeThis.length - 2} others like this`
+
+    switch(peopleWhoLikeThis.length) {
+        case 0:
+            return 'no one likes this';
+            break;
+        case 1:
+            return `${peopleWhoLikeThis[0]} likes this`;
+            break;
+        case 2:
+            return `${peopleWhoLikeThis[0]} and ${peopleWhoLikeThis[1]} like this`;
+            break;
+        case 3:
+            return `${peopleWhoLikeThis[0]}, ${peopleWhoLikeThis[1]} and ${peopleWhoLikeThis[2]} like this`
+            break;
+        default:
+            return `${peopleWhoLikeThis[0]}, ${peopleWhoLikeThis[1]} and ${peopleWhoLikeThis.length - 2} others like this`;
     }
 }
 
