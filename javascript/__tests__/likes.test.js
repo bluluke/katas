8d7668c0922a90dbbe746b28c00cb59cacfcd5d5
bluluke <bluluke@hotmail.com>
2023-08-31 10:52:22 +0100

    Test Pass: likes not alter input.

--- javascript/__tests__/likes.test.js
+++ javascript/__tests__/likes.test.js
@@ -2,14 +2,14 @@ const { likes } = require('../likes')
 
 describe('Likes', () => { 
     test('Should return message no one likes this from input of empty array', () => { 
-    //arrange 
-    const expected = 'no one likes this'; 
-    const input = []; 
-    //act 
-    const actual = likes(input); 
-    //assert 
-    expect(actual).toBe(expected); 
-    });
+        //arrange 
+        const expected = 'no one likes this'; 
+        const input = []; 
+        //act 
+        const actual = likes(input); 
+        //assert 
+        expect(actual).toBe(expected); 
+        });
     test('Should return Emma likes this from input of array containing string "Emma"', () => { 
       //arrange 
       const expected = 'Emma likes this'; 
@@ -53,21 +53,30 @@ describe('Likes', () => {
           expect(actual2).toBe(expected2); 
           expect(actual3).toBe(expected3); 
     });
-        test('Should return appropriate message from input array of 4 or more strings', () => { 
-                //arrange 
-            const expected1 = 'Frank, Sandra and 2 others like this'; 
-            const expected2 = 'Sarah, Kevin and 5 others like this'; 
-            const expected3 = 'Paul, Hannah and 7 others like this'; 
-            const input1 = ['Frank', 'Sandra', 'Barbera', 'Jessica']; 
-            const input2 = ['Sarah', "Kevin", 'Jessica', 'Frank', 'Sandra', 'Barbera', 'Deborah']; 
-            const input3 = ['Paul', 'Hannah', 'Deborah', 'Sarah', "Kevin", 'Jessica', 'Frank', 'Sandra', 'Barbera']; 
-            //act 
-            const actual1 = likes(input1); 
-            const actual2 = likes(input2); 
-            const actual3 = likes(input3); 
-            //assert 
-            expect(actual1).toBe(expected1); 
-            expect(actual2).toBe(expected2); 
-            expect(actual3).toBe(expected3); 
-        }); 
+    test('Should return appropriate message from input array of 4 or more strings', () => { 
+            //arrange 
+        const expected1 = 'Frank, Sandra and 2 others like this'; 
+        const expected2 = 'Sarah, Kevin and 5 others like this'; 
+        const expected3 = 'Paul, Hannah and 7 others like this'; 
+        const input1 = ['Frank', 'Sandra', 'Barbera', 'Jessica']; 
+        const input2 = ['Sarah', "Kevin", 'Jessica', 'Frank', 'Sandra', 'Barbera', 'Deborah']; 
+        const input3 = ['Paul', 'Hannah', 'Deborah', 'Sarah', "Kevin", 'Jessica', 'Frank', 'Sandra', 'Barbera']; 
+        //act 
+        const actual1 = likes(input1); 
+        const actual2 = likes(input2); 
+        const actual3 = likes(input3); 
+        //assert 
+        expect(actual1).toBe(expected1); 
+        expect(actual2).toBe(expected2); 
+        expect(actual3).toBe(expected3); 
+    }); 
+    test('Should not alter input', () => { 
+        //arrange
+        const input = ['Frank', 'Sandra', 'Barbera', 'Jessica']; 
+        const copyInput = ['Frank', 'Sandra', 'Barbera', 'Jessica'];
+        //act 
+        likes(input)
+        //assert 
+        expect(input).toEqual(copyInput); 
     });
+});
