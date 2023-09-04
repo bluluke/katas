941f50df39075731b8c6ffc0cecc6c5018765db3
bluluke <bluluke@hotmail.com>
2023-09-04 13:09:39 +0100

    Test Pass: isUpperCase return false from input not equal to uppercase character.

--- javascript/__tests__/isUpperCase.test.js
+++ javascript/__tests__/isUpperCase.test.js
@@ -17,4 +17,25 @@ describe.only('isUpperCase', () => {
     expect(actual2).toBe(expected); 
     expect(actual3).toBe(expected); 
   }); 
+  test('Should return false from input that is not upper case letter', () => { 
+    //arrange 
+    const expected = false; 
+    const input1 = 'g'; 
+    const input2 = 'o'; 
+    const input3 = 5; 
+    const input4 = '/'; 
+    const input5 = '&'; 
+    //act 
+    const actual1 = isUpperCase(input1); 
+    const actual2 = isUpperCase(input2); 
+    const actual3 = isUpperCase(input3); 
+    const actual4 = isUpperCase(input4); 
+    const actual5 = isUpperCase(input5); 
+    //assert 
+    expect(actual1).toBe(expected); 
+    expect(actual2).toBe(expected); 
+    expect(actual3).toBe(expected); 
+    expect(actual4).toBe(expected); 
+    expect(actual5).toBe(expected); 
+  });
 });
