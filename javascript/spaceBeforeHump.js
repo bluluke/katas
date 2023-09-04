66b3388a506631e1bf8fdc7d8a0f27c8fc3d3160
bluluke <bluluke@hotmail.com>
2023-09-04 12:50:17 +0100

    Add: isUpperCase util function for spaceBeforeHump

--- javascript/spaceBeforeHump.js
+++ javascript/spaceBeforeHump.js
@@ -3,6 +3,9 @@ const spaceBeforeHump = (inputString) => {
     return stringToChange;
 }
 
+// util function for spaceBeforeHump
+function isUpperCase() {
 
+}
 
 module.exports = { spaceBeforeHump };
