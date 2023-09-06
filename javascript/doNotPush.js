753fa43e916235b01c4ca5db8483fb69f0c3c0f4
bluluke <bluluke@hotmail.com>
2023-09-06 16:31:41 +0100

    Fix: countWhiteSpace

---
+++ javascript/doNotPush.js
@@ -0,0 +1,43 @@
+// test('Should return ', () => { 
+//         //arrange 
+//         const expected1 = ; 
+//         const expected2 = ; 
+//         const expected3 = ; 
+    
+//         const input1A = ; 
+//         const input1B = ; 
+    
+//         const input2A = ; 
+//         const input2B = ; 
+    
+//         const input3A = ; 
+//         const input3B = ; 
+//         //act 
+//         const actual1 = countInNestedArrays(input1A, input1B); 
+//         const actual2 = countInNestedArrays(input2A, input2B); 
+//         const actual3 = countInNestedArrays(input3A, input3B); 
+//         //assert 
+//         expect(actual1).toBe(expected1); 
+//         expect(actual2).toBe(expected2); 
+//         expect(actual3).toBe(expected3); 
+//       });
+
+
+// test('', () => { 
+        // //arrange 
+        // const expected1 = ; 
+        // const expected2 = ; 
+        // const expected = ; 
+    
+        // const input1 = ; 
+        // const input2 = ; 
+        // const input = ; 
+        // //act 
+        // const actual1 = maxSequence(input); 
+        // const actual2 = maxSequence(input);
+        // const actual = maxSequence(input);
+        // //assert 
+        // expect(actual1).toEqual(expected1); 
+        // expect(actual2).toEqual(expected2); 
+        // expect(actual).toEqual(expected); 
+//   });
