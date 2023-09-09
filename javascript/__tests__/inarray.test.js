53544a9387839d0599aba91e93775b88e0e70279
bluluke <bluluke@hotmail.com>
2023-09-09 10:38:04 +0100

    Update: inArray return multiple elements from first argument array when substrings of elements in second argument array.

--- javascript/__tests__/inarray.test.js
+++ javascript/__tests__/inarray.test.js
@@ -47,4 +47,27 @@ describe('inArray', () => {
     expect(actual2).toEqual(expected2); 
     expect(actual3).toEqual(expected3); 
   }); 
+  test('Should take 2 multiple element arrays and return all the first argument arrays elements which are substrings of elements in second argument.', () => { 
+    //arrange 
+    const expected1 = ['ar', 'pple']; 
+    const expected2 = ['anana', 'rain', 'us']; 
+    const expected3 = ['elicopter', 'lane', 'oat', 'range']; 
+
+    const input1a = ['ar', 'pple']; 
+    const input1b = ['car', 'apple',]; 
+
+    const input2a = ['anana', 'rain', 'us']; 
+    const input2b = ['banana', 'train', 'bus']; 
+
+    const input3a = ['elicopter', 'lane', 'oat', 'range']; 
+    const input3b = ['helicopter', 'plane', 'boat', 'orange']; 
+    //act 
+    const actual1 = inArray(input1a, input1a); 
+    const actual2 = inArray(input2a, input2b); 
+    const actual3 = inArray(input3a, input3b); 
+    //assert 
+    expect(actual1).toEqual(expected1); 
+    expect(actual2).toEqual(expected2); 
+    expect(actual3).toEqual(expected3); 
+  }); 
 });
--- javascript/inArray.js
+++ javascript/inArray.js
@@ -1,8 +1,12 @@
 const inArray = (a1, a2) => {
     let resultArray = [];
-    if(a2[0].includes(a1[0])) {
-        resultArray.push(a1[0])
-    }
+    a1.forEach((a1str) =>{ 
+        a2.forEach((a2str) => {
+            if(a2str.includes(a1str)) {
+                resultArray.push(a1str)
+            }
+        })
+    })
     return resultArray;
 }
 
