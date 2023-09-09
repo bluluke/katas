cef794431d44b34d60f2c1d1bc2f8fdd62c34a14
bluluke <bluluke@hotmail.com>
2023-09-09 10:27:26 +0100

    Test pass: inArray return single element from single element first input array when subset of single element from second input array.

--- javascript/__tests__/inarray.test.js
+++ javascript/__tests__/inarray.test.js
@@ -24,4 +24,27 @@ describe('inArray', () => {
     expect(actual2).toEqual(expected2); 
     expect(actual3).toEqual(expected3); 
   }); 
+  test('Should take 2 matching single element arrays and return the single element', () => { 
+    //arrange 
+    const expected1 = ['pple']; 
+    const expected2 = ['anana']; 
+    const expected3 = ['range']; 
+
+    const input1a = ['pple']; 
+    const input1b = ['apple']; 
+
+    const input2a = ['anana']; 
+    const input2b = ['banana']; 
+
+    const input3a = ['range']; 
+    const input3b = ['orange']; 
+    //act 
+    const actual1 = inArray(input1a, input1a); 
+    const actual2 = inArray(input2a, input2b); 
+    const actual3 = inArray(input3a, input3b); 
+    //assert 
+    expect(actual1).toEqual(expected1); 
+    expect(actual2).toEqual(expected2); 
+    expect(actual3).toEqual(expected3); 
+  }); 
 });
