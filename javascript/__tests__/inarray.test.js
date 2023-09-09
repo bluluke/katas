0fa47824156a0be2cf07d8bd506079f686469e9b
bluluke <bluluke@hotmail.com>
2023-09-09 10:23:48 +0100

    Update: inArray return single element from input of two matching single element arrays.

--- javascript/__tests__/inarray.test.js
+++ javascript/__tests__/inarray.test.js
@@ -1,1 +1,27 @@
 const {inArray} = require('../inArray');
+
+describe('Should take 2 matching single element arrays and return the single element', () => { 
+  test('', () => { 
+    //arrange 
+    const expected1 = ['apple']; 
+    const expected2 = ['banana']; 
+    const expected3 = ['orange']; 
+
+    const input1a = ['apple']; 
+    const input1b = ['apple']; 
+
+    const input2a = ['banana']; 
+    const input2b = ['banana']; 
+
+    const input3a = ['orange']; 
+    const input3b = ['orange']; 
+    //act 
+    const actual1 = inArray(input1a, input1a); 
+    const actual2 = inArray(input2a, input2b); 
+    const actual3 = inArray(input3a, input3b); 
+    //assert 
+    expect(actual1).toEqual(expected1); 
+    expect(actual2).toEqual(expected2); 
+    expect(actual3).toEqual(expected3); 
+  }); 
+});
--- javascript/inArray.js
+++ javascript/inArray.js
@@ -1,5 +1,9 @@
 const inArray = (a1, a2) => {
-
+    let resultArray = [];
+    if(a2[0].includes(a1[0])) {
+        resultArray.push(a1[0])
+    }
+    return resultArray;
 }
 
 
