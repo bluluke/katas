5c803fa1fb08225ade9325b95c9f8daea2c53754
bluluke <bluluke@hotmail.com>
2023-09-09 11:55:35 +0100

    Test Pass: countInstances return number of  elements from non-uniform first argument array which match second argument target element.

--- javascript/__tests__/countInstances.test.js
+++ javascript/__tests__/countInstances.test.js
@@ -45,4 +45,27 @@ describe('countInstances', () => {
  expect(actual2).toBe(expected2); 
  expect(actual3).toBe(expected3); 
     });
+    test('Should return number of elements from non-uniform first argument array which match second argument target element.', () => { 
+        //arrange 
+  const expected1 = 3; 
+  const expected2 = 4; 
+  const expected3 = 5; 
+ 
+  const input1a = ['a', 'b', 'a', 'c', 'a']; 
+  const input1b = 'a'; 
+ 
+  const input2a = ['b', 'a', 'b', 'c', 'b', 'b', 'd']; 
+    const input2b = 'b'; 
+   
+    const input3a = ['c', 'd', 'a', 'c', 'g', 'c', 'h', 'c', 'c']; 
+    const input3b = 'c'; 
+  //act 
+  const actual1 = countInstances(input1a, input1b); 
+  const actual2 = countInstances(input2a, input2b); 
+  const actual3 = countInstances(input3a, input3b); 
+  //assert 
+  expect(actual1).toBe(expected1); 
+  expect(actual2).toBe(expected2); 
+  expect(actual3).toBe(expected3); 
+     });
   });
