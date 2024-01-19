b6cf5211ec115e22501ee3fb14692d6404d92efd
bluluke <bluluke@hotmail.com>
2024-01-19 15:55:50 +0000

    Test pass: return single name in array from input of array containign single 4 letter string.

--- javascript/__tests__/isAFriend.test.js
+++ javascript/__tests__/isAFriend.test.js
@@ -10,4 +10,22 @@ describe('isAFriend', () => {
         //assert
         expect(actual).toEqual(expected);
     })
+    test('Should return single name in array from input of array containing single 4 letter string', () => {
+        //arrange
+        const expected1 = ['Jack'];
+        const expected2 = ['Tara'];
+        const expected3 = ['Lola'];
+
+        const input1 = ['Jack'];
+        const input2 = ['Tara'];
+        const input3 = ['Lola'];
+        //act
+        const actual1 = isAFriend(input1);
+        const actual2 = isAFriend(input2);
+        const actual3 = isAFriend(input3);
+        //assert
+        expect(actual1).toEqual(expected1);
+        expect(actual2).toEqual(expected2);
+        expect(actual3).toEqual(expected3);
+    })
 })
