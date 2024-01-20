ffb9ef81ce666714e3e3a6b2ed783ef3c19d9c8c
bluluke <bluluke@hotmail.com>
2024-01-20 15:09:00 +0000

    Test pass: return message if input not advancing start population towards targetPopulation.

--- javascript/__tests__/numOfYears.test.js
+++ javascript/__tests__/numOfYears.test.js
@@ -14,5 +14,45 @@ describe('numOfYears', () => {
         //assert
         expect(actual).toBe(expected);
     });
-    
+    test('Should return message if neither increasePercentage nor numComingOrGoing progress numOfYears towards targetPopulation', () => {
+        //arrange
+        const expected = 'Please enter valid inputs';
+
+        const input1A = 1000;
+        const input1B = 0;
+        const input1C = 0;
+        const input1D = 1001;
+
+        const input2A = 1000;
+        const input2B = -1;
+        const input2C = 0;
+        const input2D = 1001;
+
+        const input3A = 1000;
+        const input3B = 1;
+        const input3C = 0;
+        const input3D = 999;
+
+        const input4A = 1000;
+        const input4B = 0;
+        const input4C = -1;
+        const input4D = 1001;
+
+        const input5A = 1000;
+        const input5B = 0;
+        const input5C = 1;
+        const input5D = 999;
+        //act
+        const actual1 = numOfYears(input1A, input1B, input1C, input1D);
+        const actual2 = numOfYears(input2A, input2B, input2C, input2D);
+        const actual3 = numOfYears(input3A, input3B, input3C, input3D);
+        const actual4 = numOfYears(input4A, input4B, input4C, input4D);
+        const actual5 = numOfYears(input5A, input5B, input5C, input5D);
+        //assert
+        expect(actual1).toBe(expected);
+        expect(actual2).toBe(expected);
+        expect(actual3).toBe(expected);
+        expect(actual4).toBe(expected);
+        expect(actual5).toBe(expected);
+    });
 })
--- javascript/numOfYears.js
+++ javascript/numOfYears.js
@@ -1,5 +1,9 @@
 const numOfYears = (startPopulation, increasePercentage, numComingOrGoing, targetPopulationNum) => {
     let numOfYearsItTakes = 0;
+    if(startPopulation < targetPopulationNum && increasePercentage <= 0 && numComingOrGoing <= 0 || startPopulation > targetPopulationNum && increasePercentage >= 0 && numComingOrGoing >= 0)  {
+        return 'Please enter valid inputs';
+    }
+
     if(startPopulation >= targetPopulationNum) {
         return numOfYearsItTakes;
     }
