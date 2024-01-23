86b98db0cb7f265a314325c1d9d37abc4e1a2f91
bluluke <bluluke@hotmail.com>
2024-01-23 14:04:04 +0000

    Test Pass: return string of parentheses corresponding to string input of both unique and matching characters.

--- javascript/__tests__/duplicateEncode.test.js
+++ javascript/__tests__/duplicateEncode.test.js
@@ -67,5 +67,33 @@ describe('duplicateEncode', () => {
         expect(actual1).toBe(expected);
         expect(actual2).toBe(expected);
         expect(actual3).toBe(expected);
+    });
+    test('Should return string of parentheses corresponding to string input of both unique and matching characters', () => {
+        //arrange
+        const expected1 = '())((';
+        const expected2 = ')(())()))';
+        const expected3 = '))())())()';
+        const expected4 = '))())())(';
+        const expected5 = ')))))))())))(';
+
+        const input1 = 'apple';
+        const input2 = '1forever1';
+        const input3 = '9success09';
+        const input4 = '!!@££$%%^';
+        const input5 = 'intelligently';
+
+        //act
+        const actual1 = duplicateEncode(input1);
+        const actual2 = duplicateEncode(input2);
+        const actual3 = duplicateEncode(input3);
+        const actual4 = duplicateEncode(input4);
+        const actual5 = duplicateEncode(input5);
+
+        //assert
+        expect(actual1).toBe(expected1);
+        expect(actual2).toBe(expected2);
+        expect(actual3).toBe(expected3);
+        expect(actual4).toBe(expected4);
+        expect(actual5).toBe(expected5);
     })
 });
