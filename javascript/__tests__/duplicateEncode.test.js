bbb68549b48738793f4286daeadeab12c27f4373
bluluke <bluluke@hotmail.com>
2024-01-23 12:28:07 +0000

    Test Pass: return "(" from input of single character.

--- javascript/__tests__/duplicateEncode.test.js
+++ javascript/__tests__/duplicateEncode.test.js
@@ -11,5 +11,23 @@ describe('duplicateEncode', () => {
 
         //assert
         expect(actual).toBe(expected);
+    });
+    test('Should return "(" from input of single character', () => {
+        //arrange
+        const expected = '(';
+        
+        const input1 = 'd';
+        const input2 = 'f';
+        const input3 = 'e';
+
+        //act
+        const actual1 = duplicateEncode(input1);
+        const actual2 = duplicateEncode(input2);
+        const actual3 = duplicateEncode(input3);
+
+        //assert
+        expect(actual1).toBe(expected);
+        expect(actual2).toBe(expected);
+        expect(actual3).toBe(expected);
     })
 });
--- javascript/duplicateEncode.js
+++ javascript/duplicateEncode.js
@@ -2,6 +2,13 @@ const duplicateEncode = (word) => {
     if(word === '') {
         return '';
     }
+    const arrayWord = word.split('');
+    let encodedWord = '';
+    for(let i = 0; i < arrayWord.length; i++) {
+        encodedWord += '(';
+    }
+    return encodedWord;
+
 }
 
 
