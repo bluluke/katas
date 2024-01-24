91ebb35c25f057099cab1c1ecf49683fd9f8b6cd
bluluke <bluluke@hotmail.com>
2024-01-24 11:01:37 +0000

    Refactor: remove if absolute difference between int inputs equal to 0/1 conditions.

--- javascript/getSum.js
+++ javascript/getSum.js
@@ -1,13 +1,6 @@
 const getSum = (int1, int2) => {
-    if(int1 === int2) {
-        return int1;
-    }
     let sum = 0;
-
-    if(Math.abs(int1 - int2) === 1) {
-    sum = int1 + int2;
-    return sum;
-    }
+    
     const sortedInputArray = [int1, int2].sort(function(a, b) {
         return a - b;
     });
