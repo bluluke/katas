c495d23e5fa517743d6a90f3a8c4038d884966b7
bluluke <bluluke@hotmail.com>
2024-01-24 10:14:33 +0000

    Test Pass: return int sum of both input ints when absolute difference between input ints equal to 1.

--- javascript/__tests__/getSum.test.js
+++ javascript/__tests__/getSum.test.js
@@ -24,4 +24,27 @@ describe('getSum', () => {
         expect(actual2).toBe(expected2);
         expect(actual3).toBe(expected3);
     });
+    test('Should return int sum of both input ints when absolute difference between input ints equal to 1.', () => {
+        //arrange
+        const expected1 = 11;
+        const expected2 = -17;
+        const expected3 = 112003;
+
+        const input1A = 5;
+        const input1B = 6;
+        const input2A = -8;
+        const input2B = -9;
+        const input3A = 56001;
+        const input3B = 56002;
+
+        //act
+        const actual1 = getSum(input1A, input1B);
+        const actual2 = getSum(input2A, input2B);
+        const actual3 = getSum(input3A, input3B);
+
+        //assert
+        expect(actual1).toBe(expected1);
+        expect(actual2).toBe(expected2);
+        expect(actual3).toBe(expected3);
+    })
 })
--- javascript/getSum.js
+++ javascript/getSum.js
@@ -2,6 +2,8 @@ const getSum = (int1, int2) => {
     if(int1 === int2) {
         return int1;
     }
+    let sum = int1 + int2;
+    return sum;
 }
 
 
