ec3ea798eaf4aa43f346ccdf3f2d45cbb5ae3f50
bluluke <bluluke@hotmail.com>
2024-01-31 10:51:20 +0000

    Add concertAdditionToBinary Java function.

---
+++ java/binaryAddition/BinaryAddition.java
@@ -0,0 +1,13 @@
+package binaryAddition;
+
+public class BinaryAddition {
+    public static String convertAdditionToBinary(int a, int b) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
