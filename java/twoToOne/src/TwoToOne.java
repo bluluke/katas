17c7e9ba2369cb7ec98204ab661d77f85e029a07
bluluke <bluluke@hotmail.com>
2024-02-01 11:06:41 +0000

    Add twoToOne Java function.

--- java/twoToOne/src/TwoToOne.java
+++ java/twoToOne/src/TwoToOne.java
@@ -1,6 +1,11 @@
 package twoToOne.src;
 
 public class TwoToOne {
+
+    public String twoToOne(String a, String b) {
+        return "";
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello TwoTwoOne Kata.");
     }
