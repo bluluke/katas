1f6d992cbc5e21a304443d742c661de88101b607
bluluke <bluluke@hotmail.com>
2024-02-01 11:24:20 +0000

    Functionality: return combined input strings.

--- java/twoToOne/src/TwoToOne.java
+++ java/twoToOne/src/TwoToOne.java
@@ -1,12 +1,27 @@
 package twoToOne.src;
 
+import java.util.stream.Collectors;
+
 public class TwoToOne {
 
     public String twoToOne(String a, String b) {
-        return "";
+
+        String combined = a + b;
+
+        String result = combined.chars()
+            .mapToObj(c -> String.valueOf((char) c))
+            .collect(Collectors.joining());
+        return result;
     }
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello TwoTwoOne Kata.");
+
+        TwoToOne twoToOne = new TwoToOne(); 
+
+        String s1 = "xyaabbbccccdefww";
+        String s2 = "xxxxyyyyabklmopq";
+        String result = twoToOne.twoToOne(s1, s2);
+
+        System.out.println("Combined and sorted result: " + result);
     }
 }
