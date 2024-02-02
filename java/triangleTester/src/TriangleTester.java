5c02df3704ed244376ca984e56bb5ae334304336
bluluke <bluluke@hotmail.com>
2024-02-02 10:41:45 +0000

    Fix: missing return statement and semicolon.

--- java/triangleTester/src/TriangleTester.java
+++ java/triangleTester/src/TriangleTester.java
@@ -3,7 +3,7 @@ package triangleTester.src;
 public class TriangleTester {
 
     public static boolean isTriangle(int a, int b, int c){
-    
+        return true;
     }
 
     public static void main(String[] args) {
