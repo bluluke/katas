a8c586fd1a7d49667a27514c439800d1c61ce35f
bluluke <bluluke@hotmail.com>
2024-02-02 11:10:35 +0000

    Functionality: return true if 3 input integers pass triangle inequality  theorem and false if not.

--- java/triangleTester/src/TriangleTester.java
+++ java/triangleTester/src/TriangleTester.java
@@ -1,12 +1,29 @@
 package triangleTester.src;
 
+
+
 public class TriangleTester {
 
     public static boolean isTriangle(int a, int b, int c){
+        int[] sideLengths = {a, b, c};
+
+        for(int i = 0; i < sideLengths.length; i++) {
+            int sum = 0;
+            for(int j = 0; j < sideLengths.length; j++) {
+                if(i != j) {
+                    sum += sideLengths[j]; 
+                }
+            }
+            if(sum <= sideLengths[i]) {
+                return false; 
+            }
+        }
+    
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello Trianlge Tester");
+        System.out.println(isTriangle(2, 3, 4));
+        System.out.println(isTriangle(20, 3, 4));
     }
 }
