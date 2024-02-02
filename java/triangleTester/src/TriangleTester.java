ed65b334095e2c5d2dd4d50b4b17d56c68569395
bluluke <bluluke@hotmail.com>
2024-02-02 10:40:35 +0000

    Add TriangleTester Java function.

---
+++ java/triangleTester/src/TriangleTester.java
@@ -0,0 +1,12 @@
+package triangleTester.src;
+
+public class TriangleTester {
+
+    public static boolean isTriangle(int a, int b, int c){
+    
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Trianlge Tester");
+    }
+}
