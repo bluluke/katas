a2ee139af0faa2b60f8038f264766d2b8999998a
bluluke <bluluke@hotmail.com>
2024-02-05 12:03:33 +0000

    Fix: spelling.

--- java/numOfMultipes/src/NumOfMultiples.java
+++ java/numOfMultipes/src/NumOfMultiples.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class NumOfMultiples {
 
-    public static List<Integer> createArrayOfMultipes(int commonFactor, int numMultiples) {
+    public static List<Integer> createArrayOfMultiples(int commonFactor, int numMultiples) {
         List<Integer> listOfMultiples = new ArrayList<>();
         for(int i = 1; i <= numMultiples; i++) {
             listOfMultiples.add(commonFactor * i);
@@ -11,9 +11,9 @@ public class NumOfMultiples {
         return listOfMultiples;
     }
     public static void main(String[] args) {
-        System.out.println(createArrayOfMultipes(5, 2));
-        System.out.println(createArrayOfMultipes(4, 10));
-        System.out.println(createArrayOfMultipes(3, 9));
+        System.out.println(createArrayOfMultiples(5, 2));
+        System.out.println(createArrayOfMultiples(4, 10));
+        System.out.println(createArrayOfMultiples(3, 9));
 
     }
 }
