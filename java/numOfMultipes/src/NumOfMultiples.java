71112bad9b1c48fefc3396679290406c82b7fe61
bluluke <bluluke@hotmail.com>
2024-02-05 11:53:42 +0000

    Functionality: return List of integer multiples corresponding to input integer common factor and input integer numOfMultiples.

--- java/numOfMultipes/src/NumOfMultiples.java
+++ java/numOfMultipes/src/NumOfMultiples.java
@@ -5,10 +5,15 @@ public class NumOfMultiples {
 
     public static List<Integer> createArrayOfMultipes(int commonFactor, int numMultiples) {
         List<Integer> listOfMultiples = new ArrayList<>();
+        for(int i = 1; i <= numMultiples; i++) {
+            listOfMultiples.add(commonFactor * i);
+        }
         return listOfMultiples;
     }
     public static void main(String[] args) {
-        System.out.println(createArrayOfMultipes(0, 0));
+        System.out.println(createArrayOfMultipes(5, 2));
+        System.out.println(createArrayOfMultipes(4, 10));
+        System.out.println(createArrayOfMultipes(3, 9));
 
     }
 }
