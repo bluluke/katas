3c98f28d52b89fb504184b34d2cf3616eed1906f
bluluke <bluluke@hotmail.com>
2024-02-05 11:47:23 +0000

    Functionality: createArrayOfMultipes return ArrayList.

--- java/numOfMultipes/src/NumOfMultiples.java
+++ java/numOfMultipes/src/NumOfMultiples.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumOfMultiples {
+
+    public static List<Integer> createArrayOfMultipes(int commonFactor, int numMultiples) {
+        List<Integer> listOfMultiples = new ArrayList<>();
+        return listOfMultiples;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello numOfMultiples");
+        System.out.println(createArrayOfMultipes(0, 0));
+
     }
 }
