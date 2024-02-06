9b260983b8395e4fb55d13ba8f81a07e61e45399
bluluke <bluluke@hotmail.com>
2024-02-06 10:25:55 +0000

    Functionality: turn input string into array.

--- java/xo/src/XO.java
+++ java/xo/src/XO.java
@@ -1,10 +1,15 @@
 public class XO {
     public static boolean getXO (String str) {
     
+        String[] arrayOfXOs = str.split(",");
+
+        for(String s: arrayOfXOs) {
+            System.out.println(s);
+        }
        return true;
         
       }
       public static void main(String[] args) {
-        System.out.println("Hello XO");
+        getXO("xoxooxx");
       }
 }
