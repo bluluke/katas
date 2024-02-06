343147b3bb7fb993a03cb08db30ef672c2308f5f
bluluke <bluluke@hotmail.com>
2024-02-06 10:42:18 +0000

    Refactor: create array of char instead of array of 1 character strings.

--- java/xo/src/XO.java
+++ java/xo/src/XO.java
@@ -1,14 +1,14 @@
 public class XO {
     public static boolean getXO (String str) {
     
-        String[] arrayOfXOs = str.split("");
+        char[] arrayOfXOs = str.toCharArray();
         int xCount = 0;
         int oCount = 0; 
 
-        for(String s: arrayOfXOs) {
-            if(s.equals("x")) {
+        for(char c: arrayOfXOs) {
+            if(c == 'x') {
                 xCount++;
-            } else if(s.equals("o")) {
+            } else if(c == 'o') {
                 oCount++;
             } 
         }
@@ -16,6 +16,7 @@ public class XO {
         
       }
       public static void main(String[] args) {
-        getXO("xoxooxx");
+        System.out.println(getXO("xoxooxx"));
+        System.out.println(getXO("xoxoox"));
       }
 }
