aafac242c5423982e56881a87185a0f4faa26888
bluluke <bluluke@hotmail.com>
2024-02-06 10:20:28 +0000

    Add XO Java Class.

---
+++ java/xo/src/XO.java
@@ -0,0 +1,10 @@
+public class XO {
+    public static boolean getXO (String str) {
+    
+       return true;
+        
+      }
+      public static void main(String[] args) {
+        System.out.println("Hello XO");
+      }
+}
