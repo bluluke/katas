646dbfe5bd3cb8fcc377d587ff9f55ad5cc15b0b
bluluke <bluluke@hotmail.com>
2024-02-13 19:20:29 +0000

    Functionality: return input int digits in descending order.

--- java/descendingOrder/src/App.java
+++ java/descendingOrder/src/App.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+
 public class App {
     public static int descendingOrder(final int num) {
-      return num;
+      String numString = Integer.toString(num);
+      char[] charArray = numString.toCharArray();
+      Arrays.sort(charArray);
+
+      StringBuilder resultBuilder = new StringBuilder(new String(charArray));
+      String resultString = resultBuilder.reverse().toString();
+
+      return Integer.parseInt(resultString);
     }
 
     public static void main(String[] args)  {
-        System.out.println("Hello descendingOrder");
+        System.out.println(descendingOrder(42145)); 
+        System.out.println(descendingOrder(145263)); 
+        System.out.println(descendingOrder(123456789));
     }
   }
