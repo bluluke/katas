4ce313b20a372acbb1bf5f3f7066591e207c9ec6
bluluke <bluluke@hotmail.com>
2024-02-13 19:15:03 +0000

    Add descendingOrder Java function.

---
+++ java/descendingOrder/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static int descendingOrder(final int num) {
+      return num;
+    }
+
+    public static void main(String[] args)  {
+        System.out.println("Hello descendingOrder");
+    }
+  }
