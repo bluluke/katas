f94b8a0dd047277ac64da9e73277a40e3ceddcd9
bluluke <bluluke@hotmail.com>
2024-02-26 15:26:45 +0000

    Add areYouPlayingBanjo Java function.

--- java/banjo/src/Banjo.java
+++ java/banjo/src/Banjo.java
@@ -1,7 +1,13 @@
 public class Banjo {
 
+    public static String areYouPlayingBanjo(String name) {
+        return name;
+    }
     public static void main(String[] args) {
         System.out.println("Hello areYouPlayingBanjo");
+        Banjo aBanjo = new Banjo();
+        aBanjo.areYouPlayingBanjo("Jack");
+
     }
     
 }
