218f9a20daab7c539362ef420b949fa351d5e275
bluluke <bluluke@hotmail.com>
2024-02-27 16:48:02 +0000

    Functionality: return square of every digit from int input and concatenate them.

--- java/squareDigit/src/SquareDigit.java
+++ java/squareDigit/src/SquareDigit.java
@@ -1,10 +1,29 @@
 public class SquareDigit {
 
     public int squareDigits(int n) {
-        return n;
+        int result = 0;
+
+        int position = 1;
+
+        while(n > 0) {
+            int digit = n % 10;
+
+            int square = digit * digit;
+
+            result = square * position + result; 
+
+            position *= 100;
+
+            n /= 10;
+        }
+        return result; 
     }
   
     public static void main(String[] args) {
-        System.out.println("Hello squareDigits!");
+
+        SquareDigit squareDigit = new SquareDigit();
+
+        System.out.println(squareDigit.squareDigits(9119)); 
+        System.out.println(squareDigit.squareDigits(765));  
     } 
   }
