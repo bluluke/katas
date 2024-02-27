fef1564eaf87de2e030ca890078fe75f1d5f25b5
bluluke <bluluke@hotmail.com>
2024-02-27 16:37:09 +0000

    Add squareDigits Java function.

---
+++ java/squareDigit/src/SquareDigit.java
@@ -0,0 +1,10 @@
+public class SquareDigit {
+
+    public int squareDigits(int n) {
+        return n;
+    }
+  
+    public static void main(String[] args) {
+        System.out.println("Hello squareDigits!");
+    } 
+  }
