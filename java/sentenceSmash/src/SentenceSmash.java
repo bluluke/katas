85c6c1febdd5ad876e8e431901bb94cc9c44992c
bluluke <bluluke@hotmail.com>
2024-02-29 19:49:47 +0000

    Functionality: return empty string from input of 0 length.

--- java/sentenceSmash/src/SentenceSmash.java
+++ java/sentenceSmash/src/SentenceSmash.java
@@ -1,9 +1,14 @@
 public class SentenceSmash {
 
 public static String smashWords(String... words) {
-    return "Hello smashWords";
+    if(words.length == 0) 
+        return "";
+        return "Hello smashWords!";
 }
     public static void main(String[] args) {
+        String[] input = new String[] {}; 
         System.out.println(smashWords("abc"));
+        System.out.println(smashWords());
+        System.out.println(smashWords(input));
     }
 }
