25deab8881beb87b81a7ec89e0b6ca009d0091c9
bluluke <bluluke@hotmail.com>
2024-02-29 19:45:04 +0000

    Add smashWords Java function.

--- java/sentenceSmash/src/SentenceSmash.java
+++ java/sentenceSmash/src/SentenceSmash.java
@@ -1,7 +1,9 @@
 public class SentenceSmash {
 
-
+public static String smashWords(String... words) {
+    return "Hello smashWords";
+}
     public static void main(String[] args) {
-        System.out.println("Hello smashWords");
+        System.out.println(smashWords("abc"));
     }
 }
