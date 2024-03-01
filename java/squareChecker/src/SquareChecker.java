a38ca5591be927e3632ed371ca500cb904a39a76
bluluke <bluluke@hotmail.com>
2024-03-01 19:37:06 +0000

    Functionality: return false if input not a square number and true otherwise.

--- java/squareChecker/src/SquareChecker.java
+++ java/squareChecker/src/SquareChecker.java
@@ -4,9 +4,18 @@ public class SquareChecker {
             if(inputNum < 0) {
                 return false;
             }
-            return true;
+
+            int sqrt = (int) Math.sqrt(inputNum); 
+
+
+            return sqrt * sqrt == inputNum;
         }
     public static void main(String[] args) {
         System.out.println(checkForSquare(-1));
+        System.out.println(checkForSquare(0)); 
+        System.out.println(checkForSquare(3));  
+        System.out.println(checkForSquare(4));  
+        System.out.println(checkForSquare(25)); 
+        System.out.println(checkForSquare(26)); 
     }
 }
