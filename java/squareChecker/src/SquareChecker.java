097e32dd6b2285d0c4554ebe06da1e0c9dde6b3f
bluluke <bluluke@hotmail.com>
2024-03-01 19:29:19 +0000

    Add checkForSquare Java function.

--- java/squareChecker/src/SquareChecker.java
+++ java/squareChecker/src/SquareChecker.java
@@ -1,6 +1,10 @@
 public class SquareChecker {
 
+        public static boolean checkForSquare(int inputNum) {
+            System.out.println("Hello checkForSquare");
+            return true;
+        }
     public static void main(String[] args) {
-        System.out.println("Hello SquareChecker!");
+        System.out.println(checkForSquare(4));
     }
 }
