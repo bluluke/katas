3c2ed80dd9f820f7c8035da3a77a8d54d7af8341
bluluke <bluluke@hotmail.com>
2024-03-02 13:29:33 +0000

    Functionality: return new array with lowest int from input array removed.

--- java/removeLowest/src/RemoveLowest.java
+++ java/removeLowest/src/RemoveLowest.java
@@ -6,13 +6,36 @@ public class RemoveLowest {
         if(numbers == null || numbers.length == 0) {
             return null;
         }
-        int[] returnArray = new int[] {1, 2, 3};
-        return returnArray;
+
+
+        int minIndex = 0;
+        for(int i = 1; i < numbers.length; i++) {
+            if(numbers[i] < numbers[minIndex]) {
+                minIndex = i; 
+            }
+        }
+
+        int[] result = new int[numbers.length - 1];
+
+        for(int i = 0, j = 0; i < numbers.length; i++) {
+            if(i != minIndex) {
+                result[j++] = numbers[i];
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
 
-        int[] intArray = new int[0];
-        System.out.println(removesLowest(intArray));
+        int[] emptyIntArray = new int[0];
+        System.out.println(removesLowest(emptyIntArray));
+
+        int[] intArray = new int[] {9, 2, 5, 87, 1};
+        int[] result2 = removesLowest(intArray);
+        for (int num : result2) {
+            System.out.print(num + " ");
+        }
+
     }
 }
