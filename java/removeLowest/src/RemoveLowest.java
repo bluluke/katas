4021659bd02cb54d1ecf6ea1cd5b77ade367a07c
bluluke <bluluke@hotmail.com>
2024-03-02 13:08:03 +0000

    Add removesLowest Java function.

--- java/removeLowest/src/RemoveLowest.java
+++ java/removeLowest/src/RemoveLowest.java
@@ -1,8 +1,13 @@
 public class RemoveLowest {
  
-    
+    public static int[] removesLowest(int[] numbers) {
+        System.out.println("Hello removesLowest!");
+        return null;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello RemoveLowest");
+        int[] intArray = new int[] {1, 2};
+        removesLowest(intArray);
     }
 }
