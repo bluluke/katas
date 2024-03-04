ced37f2ee7a3eafefe3e93691e89a9312dfc27e1
bluluke <bluluke@hotmail.com>
2024-03-04 19:33:40 +0000

    Functionality: doublesString return string containing each character from input string doubled.

--- java/doubleString/src/DoubleString.java
+++ java/doubleString/src/DoubleString.java
@@ -2,12 +2,20 @@ public class DoubleString {
 
 
     public static String doublesString(String str) {
-        System.out.println("Hello doublesString!");
-        return str; 
+       StringBuilder result = new StringBuilder();
+       for(int i = 0; i < str.length(); i++) {
+        char c = str.charAt(i);
+        result.append(c).append(c);
+       }
+        return result.toString(); 
     }
     public static void main(String[] args) {
-        String str = "abcde";
-        doublesString(str);
+        String str1 = "abcde";
+        System.out.println(doublesString(str1));
+        String str2 = "I love carrots!";
+        System.out.println(doublesString(str2));
+        String str3 = "1235678 67";
+        System.out.println(doublesString(str3));
     }
 
 }
