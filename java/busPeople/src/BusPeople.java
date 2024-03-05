c8f6bb9bae63d6fe60c283052a0727c500ed8d45
bluluke <bluluke@hotmail.com>
2024-03-05 14:18:28 +0000

    Functionality: return total of 2 integer elements in input ArrayList.

--- java/busPeople/src/BusPeople.java
+++ java/busPeople/src/BusPeople.java
@@ -5,11 +5,23 @@ import java.util.List;
 public class BusPeople {
 
 
-    public static int countPassengers() {
-        System.out.println("Hello countPassengers!");
-        return 0; 
+    public static int countPassengers(ArrayList<int[]> stops) {
+        int totalPassengers = 0;
+
+        for(int[] stop : stops) {
+            totalPassengers += stop[0];
+            totalPassengers -= stop[1]; 
+        }
+        return totalPassengers; 
     }
     public static void main(String[] args) {
-        System.out.println(countPassengers());
+
+        ArrayList<int[]> stops = new ArrayList<>();
+        stops.add(new int[]{10,0});
+        stops.add(new int[]{3,5});
+        stops.add(new int[]{15,8});
+
+        int totalPassengers = countPassengers(stops);
+        System.out.println("Total number of people on the bus: " + totalPassengers); 
     }
 }
