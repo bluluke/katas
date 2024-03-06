84352955433c1b101f60facb1a4c0edd79fb0eb0
bluluke <bluluke@hotmail.com>
2024-03-06 20:08:19 +0000

    Add getMiddleCharacter JAva function.

--- java/middleCharacter/src/MiddleCharacter.java
+++ java/middleCharacter/src/MiddleCharacter.java
@@ -1,8 +1,11 @@
 public class MiddleCharacter {
 
+    public static String getMiddleCharacter(String word) {
+        return "Hello getMiddleCharacter";
+    }
     
     public static void main(String[] args) {
-       System.out.println("Hello MiddleCharacter!");
+       System.out.println(getMiddleCharacter("hey"));
     }
 
 
