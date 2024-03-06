bd005ae8cbe4f62ecd31389aa4ee52a3a8832a0d
bluluke <bluluke@hotmail.com>
2024-03-06 20:09:40 +0000

    Functionality: return middle character/characters from input string.

--- java/middleCharacter/src/MiddleCharacter.java
+++ java/middleCharacter/src/MiddleCharacter.java
@@ -1,11 +1,20 @@
 public class MiddleCharacter {
 
     public static String getMiddleCharacter(String word) {
-        return "Hello getMiddleCharacter";
+        int length = word.length();
+        if (length % 2 == 0) {
+            int mid = length / 2;
+            return word.substring(mid - 1, mid + 1);
+        } else {
+            int mid = length / 2;
+            return String.valueOf(word.charAt(mid));
+        }
     }
     
     public static void main(String[] args) {
-       System.out.println(getMiddleCharacter("hey"));
+        System.out.println(getMiddleCharacter("word"));
+        System.out.println(getMiddleCharacter("java")); 
+        System.out.println(getMiddleCharacter("programming"));
     }
 
 
