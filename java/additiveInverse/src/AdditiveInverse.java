60c6b8acf081001fc1f23fab93a8376142b7854c
bluluke <bluluke@hotmail.com>
2024-03-07 19:59:21 +0000

    Functionality: return double additive inverse of input double.

--- java/additiveInverse/src/AdditiveInverse.java
+++ java/additiveInverse/src/AdditiveInverse.java
@@ -1,11 +1,13 @@
 public class AdditiveInverse {
     
     public static double opposite(double number) {
-        System.out.println("Hello opposite!"); 
-        return number; 
+        return -number;
     }
 
     public static void main(String[] args) {
         System.out.println(opposite(2));
+        System.out.println(opposite(1));   
+        System.out.println(opposite(14));  
+        System.out.println(opposite(-34)); 
     }
 }
