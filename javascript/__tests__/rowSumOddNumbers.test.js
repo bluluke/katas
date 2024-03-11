e74a293a7255dbd8b8ff27e86f272b971e6cefee
bluluke <bluluke@hotmail.com>
2024-03-11 17:35:47 +0000

    Test Passed: from triangle represented by ascending odd numbers, return the sum of row indicated by input integer.

--- javascript/__tests__/rowSumOddNumbers.test.js
+++ javascript/__tests__/rowSumOddNumbers.test.js
@@ -18,5 +18,27 @@ describe('rowSumOddNumbers', () => {
         const actual = rowSumOddNumbers(input);
         //assert
         expect(actual).toBe(expected);
+    });
+    test('from a triangle comprised of ascending odd numbers, return the sum of the row indicated by the input integer', () => {
+        //act
+        const expected1 = 27 ;
+        const expected2 = 64;
+        const expected3 = 125;
+        const expected4 = 216;
+
+        const input1 = 3;
+        const input2 = 4;
+        const input3 = 5;
+        const input4 = 6;
+        //act
+        const actual1 = rowSumOddNumbers(input1);
+        const actual2 = rowSumOddNumbers(input2);
+        const actual3 = rowSumOddNumbers(input3);
+        const actual4 = rowSumOddNumbers(input4);
+        //assert
+        expect(actual1).toBe(expected1);
+        expect(actual2).toBe(expected2);
+        expect(actual3).toBe(expected3);
+        expect(actual4).toBe(expected4);
     })
 })
--- javascript/rowSumOddNumbers.js
+++ javascript/rowSumOddNumbers.js
@@ -1,13 +1,24 @@
 const rowSumOddNumbers = (n) => {
-    switch(n) {
-        case 1:
-            return 1;
-            break;
-        case 2:
-            return 8;
-            break;
-    }
-  
+
+    let numToSkip = 0; 
+ 
+for(let i = 1; i <= n; i++) {
+    if(i !== n) {
+        numToSkip += i; 
+    } 
+}
+
+let oddNum = 1;
+for(let i = 0; i < numToSkip; i++) {
+    oddNum +=2;
+}
+
+let total = 0;
+for(let i = 1; i <= n; i++) {
+    total += oddNum;
+    oddNum += 2;
+}
+return total;
 }
 
 
