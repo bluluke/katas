0207f368c8772a2e8979a7b9415a97e4d974569b
bluluke <bluluke@hotmail.com>
2024-03-14 18:23:53 +0000

    Functionality: return false unless employed input equal to true and onVacation input equal to false.

--- c_sharp/setAlarm/Program.cs
+++ c_sharp/setAlarm/Program.cs
@@ -4,11 +4,22 @@
 
 class Program 
 {
-    public static void SetAlarm() {
-    Console.WriteLine("Hello, SetAlarm!");
+    public static bool SetAlarm(bool employed, bool onVacation) {
+    if(!employed) {
+        return false;
+    } else {
+        if(onVacation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
     static void Main(string[] args)
     {
-        SetAlarm();
+        Console.WriteLine(SetAlarm(true, true));
+        Console.WriteLine(SetAlarm(false, true));
+        Console.WriteLine(SetAlarm(false, false));
+        Console.WriteLine(SetAlarm(true, false));
     }
 }
