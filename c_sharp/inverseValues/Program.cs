b9d154284f4d0cc319017c1abea6b73e7bd1a3dc
bluluke <bluluke@hotmail.com>
2024-03-15 07:38:41 +0000

    Add ArraysInversion C# function.

--- c_sharp/inverseValues/Program.cs
+++ c_sharp/inverseValues/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+
 class Program {
+    public static void ArraysInversion() {
+    Console.WriteLine("Hello, ArraysInversion");
+}
     static void Main(string[] args) {
-        Console.WriteLine("Hello, ArraysInversion Program!");
+        ArraysInversion();
     }
 }
