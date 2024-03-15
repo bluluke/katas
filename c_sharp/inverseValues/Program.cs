39c25caa9e6d6309f7787b20d86056495c762443
bluluke <bluluke@hotmail.com>
2024-03-15 08:18:16 +0000

    Refactor: ArraysInversion have no conditional logic.

--- c_sharp/inverseValues/Program.cs
+++ c_sharp/inverseValues/Program.cs
@@ -5,17 +5,8 @@ class Program {
     public static List<int> ArraysInversion(int[] numArray) {
         List<int> inverseArray = new List<int>(); 
         foreach(int num in numArray) {
-            if(num < 0) {
-            inverseArray.Add(Math.Abs(num));
-            }
-        
-        else if(num > 0) {
-            int negNum = num - num - num;
-            inverseArray.Add(negNum);
-        } else {
-            inverseArray.Add(num);
+            inverseArray.Add(-num);
         }
-    }
 return inverseArray;
 }
 
