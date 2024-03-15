46d4b96b7c40e172a2745fbf09b89c8f5cea989d
bluluke <bluluke@hotmail.com>
2024-03-15 08:16:14 +0000

    Functionality: return list of inverse integersfrom input array of integers.

--- c_sharp/inverseValues/Program.cs
+++ c_sharp/inverseValues/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-
+using System.Linq;
 
 class Program {
-    public static void ArraysInversion() {
-    Console.WriteLine("Hello, ArraysInversion");
-}
-    static void Main(string[] args) {
-        ArraysInversion();
+    public static List<int> ArraysInversion(int[] numArray) {
+        List<int> inverseArray = new List<int>(); 
+        foreach(int num in numArray) {
+            if(num < 0) {
+            inverseArray.Add(Math.Abs(num));
+            }
+        
+        else if(num > 0) {
+            int negNum = num - num - num;
+            inverseArray.Add(negNum);
+        } else {
+            inverseArray.Add(num);
+        }
     }
+return inverseArray;
+}
+
+    static void Main(string[] args) {
+        int[] arrayOfInts = new int[] {1, 4, 6, -5, -9, 0};
+        List<int> result = ArraysInversion(arrayOfInts);
+        foreach(int num in result) {
+            Console.WriteLine(num);
+        }
+    }
+
 }
