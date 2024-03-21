d9b4a19c8dd4050744310f647839db001b4b601c
bluluke <bluluke@hotmail.com>
2024-03-21 12:02:56 +0000

    Functionality: return number from fibonacci sequence specified by input integer.

--- c_sharp/fibonacci/Program.cs
+++ c_sharp/fibonacci/Program.cs
@@ -3,10 +3,24 @@
 
 class Program {
 
-    public static void Fibonacci() {
-        Console.WriteLine("Hello, Fibonacci!");
+    public static int Fibonacci(int num) {
+        if(num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
+        }
+        else {
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
     }
     static void Main(string[] args) {
-        Fibonacci();
+        Console.WriteLine(Fibonacci(0));
+        Console.WriteLine(Fibonacci(1));
+        Console.WriteLine(Fibonacci(2));
+        Console.WriteLine(Fibonacci(3));
+        Console.WriteLine(Fibonacci(4));
+        Console.WriteLine(Fibonacci(5));
+        Console.WriteLine(Fibonacci(6));
+        Console.WriteLine(Fibonacci(7));
     }
 }
