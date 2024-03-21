479eb66f5acd4780ce733714e268f4409819f23d
bluluke <bluluke@hotmail.com>
2024-03-21 11:39:46 +0000

    Functionality: return factorial of input integer.

--- c_sharp/factorial/Program.cs
+++ c_sharp/factorial/Program.cs
@@ -3,10 +3,19 @@
 
 class Program {
 
-    public static void Factorial() {
-        Console.WriteLine("Hello, Factorial!");
+    public static int Factorial(int num) {
+        if(num == 0) {
+            return 1;
+        } else {
+            return num * Factorial( num - 1);
+        }
     }
     static void Main(string[] args) {
-        Factorial();
+        Console.WriteLine(Factorial(0));
+        Console.WriteLine(Factorial(1));
+        Console.WriteLine(Factorial(2));
+        Console.WriteLine(Factorial(3));
+        Console.WriteLine(Factorial(4));
+        Console.WriteLine(Factorial(5));
     }
 }
