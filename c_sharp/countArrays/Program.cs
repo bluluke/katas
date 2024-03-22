d61f42ead34ae92663bc7a004e2b82d3efa2ec4c
bluluke <bluluke@hotmail.com>
2024-03-22 15:24:23 +0000

    Functionality: return integer number of  arrays in input nested array.

--- c_sharp/countArrays/Program.cs
+++ c_sharp/countArrays/Program.cs
@@ -3,10 +3,36 @@
 
 class Program {
 
-    public static void CountArrays() {
-        Console.WriteLine("Hello, CountArrays!");
+    public static int CountArrays(object[] array) {
+        int count = 0; 
+        foreach (var item in array) {
+            if(item is object[]) {
+                count++;
+                count += CountArrays((object[])item);
+            }
+        }
+        return count; 
     }
     static void Main(string[] args) {
-        CountArrays();
+
+        object[] nestedArray = new object[]
+        {
+            1,
+            new object[] {2, 3},
+            new object[] { 4, new object[] { 5, 6, new object[] {7, 8 }}}
+        };
+        int arrayCount = CountArrays(nestedArray);
+
+        object[] anotherNestedArray = new object[]
+{
+    "A",
+    new object[] { "B", "C" },
+    new object[] { "D", new object[] { "E", "F", new object[] {"G", "H", new object[] { "I", "J" } } } },
+    new object[] { "K", "L", new object[] { "M", "N", new object[] {"O", "P" } } }
+};
+int anotherArrayCount = CountArrays(anotherNestedArray);
+        Console.WriteLine(arrayCount);
+        Console.WriteLine(anotherArrayCount);
+
     }
 }
