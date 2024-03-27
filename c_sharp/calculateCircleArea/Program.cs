331c0eae66fead5e7df87961c50a80161c7f1c0f
bluluke <bluluke@hotmail.com>
2024-03-27 21:20:53 +0000

    Functionality: return area of circle as a double  from input double representing radius.

--- c_sharp/calculateCircleArea/Program.cs
+++ c_sharp/calculateCircleArea/Program.cs
@@ -4,10 +4,13 @@
 class Program {
 
 
-    public static void CalculateCircleArea() {
-        Console.WriteLine("Hello, CalculateCircleArea!"); 
+    public static double CalculateCircleArea(double radius) {
+        double area = Math.PI * radius * radius; 
+        return area;
     }
     static void Main(string[] args) {
-        CalculateCircleArea();
+        Console.WriteLine(CalculateCircleArea(5.0));
+        Console.WriteLine(CalculateCircleArea(7.0));
+        Console.WriteLine(CalculateCircleArea(20.5));
     }
 }
