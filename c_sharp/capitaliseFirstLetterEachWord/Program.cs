84d6e16244934c375ad6e6aeba6175d7a82f4c65
bluluke <bluluke@hotmail.com>
2024-03-29 10:53:05 +0000

    Functionality: return input string in title case.

--- c_sharp/capitaliseFirstLetterEachWord/Program.cs
+++ c_sharp/capitaliseFirstLetterEachWord/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
-
+using System.Globalization; 
 class Program {
 
 
-    public static void CapitaliseFirstLetterEachWord() {
-        Console.WriteLine("Hello, CapitaliseFirstLetterEachWord!");
+    public static string CapitaliseFirstLetterEachWord(string inputString) {
+        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        string[] words = inputString.Split(' '); 
+
+        for(int i = 0; i < words.Length; i++) {
+            words[i] = textInfo.ToTitleCase(words[i]);
+        }
+
+        string result = string.Join(" ", words);
+
+        return result; 
     }
 
     static void Main(string[] args) {
-        CapitaliseFirstLetterEachWord();
+        Console.WriteLine(CapitaliseFirstLetterEachWord("eggs and spinach."));
     }
 }
