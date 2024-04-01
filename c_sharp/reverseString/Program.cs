7e475e63fc644c36f811437f72aeb25011e1049a
bluluke <bluluke@hotmail.com>
2024-04-01 17:14:54 +0100

    Functionality: return input string reversed.

--- c_sharp/reverseString/Program.cs
+++ c_sharp/reverseString/Program.cs
@@ -2,10 +2,15 @@
 
 class Program {
 
-    public static void ReverseString() {
-        Console.WriteLine("Hello, ReverseString!");
+    public static string ReverseString(string inputString) {
+        char[] charArray = inputString.ToCharArray();
+        Array.Reverse(charArray); 
+        string reversedString = new string(charArray);
+        return reversedString;
     }
     static void Main(string[] args) {
-        ReverseString();
+        Console.WriteLine(ReverseString("eggs"));
+        Console.WriteLine(ReverseString("cheese and onion"));
+        Console.WriteLine(ReverseString("1 2 3 4 5 6 7 8 9 10"));
     }
 }
