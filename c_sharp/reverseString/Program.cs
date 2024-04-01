af79355ac283d1a7e8cc1525ef41ec04178fa2d8
bluluke <bluluke@hotmail.com>
2024-04-01 17:09:21 +0100

    Add C# function ReverseString.

--- c_sharp/reverseString/Program.cs
+++ c_sharp/reverseString/Program.cs
@@ -2,7 +2,10 @@
 
 class Program {
 
+    public static void ReverseString() {
+        Console.WriteLine("Hello, ReverseString!");
+    }
     static void Main(string[] args) {
-        Console.WriteLine("Hello, ReverseString Program!");
+        ReverseString();
     }
 }
